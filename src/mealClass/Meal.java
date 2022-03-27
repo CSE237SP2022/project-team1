ad5ada5f0f4d22bb29902121395cd4ee80f3175e
Maddie Hoffmann <e.hoffmann@wustl.edu>
2022-03-27 11:02:13 -0500

    Pushed meal class

---
+++ src/mealClass/Meal.java
@@ -0,0 +1,60 @@
+package mealClass;
+
+import java.util.ArrayList;
+
+import foodClass.Food;
+
+
+public class Meal {
+
+	ArrayList<Food> mealItems = new ArrayList<Food>();
+	String name;
+	
+	public Meal() {
+		this.name = name;
+		this.mealItems = mealItems;
+	}
+	
+	public void addFood(Food food) {
+		mealItems.add(food);
+	}
+	
+	public int totalCalories() {
+		
+		int totalCaloriesInMeal = 0;
+		for(Food f : mealItems) {
+			
+			totalCaloriesInMeal += f.getCalories();
+		}
+		
+		return totalCaloriesInMeal;
+	}
+	
+	public double totalCarbs() {
+		
+		double totalCarbsInMeal = 0;
+		for(Food f : mealItems) {
+			
+			totalCarbsInMeal += f.getCarbs();
+		}
+		
+		return totalCarbsInMeal;
+	}
+	
+	public int numberOfFoodItems() {
+		
+		return mealItems.size();
+	}
+	
+	public double totalFat() {
+		
+		double totalFatInMeal = 0;
+		for(Food f : mealItems) {
+			
+			totalFatInMeal += f.getFat();
+		}
+		
+		return totalFatInMeal;
+	
+	}
+}
