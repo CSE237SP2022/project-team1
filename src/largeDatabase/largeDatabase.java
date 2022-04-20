3a31ecfbdc9c0b76df6b65ccc56279e05ecd791f
obiekahne <39808077+obiekahne@users.noreply.github.com>
2022-04-20 15:04:50 -0500

    adding branch

---
+++ src/largeDatabase/largeDatabase.java
@@ -0,0 +1,13 @@
+package largeDatabase;
+
+import java.io.File;
+
+public class largeDatabase {
+	
+	
+	String curDir = System.getProperty("user.dir");
+	String fileName = "largeDatabase.csv";
+	File largeData = new File(curDir + "/" + fileName);
+	 
+
+}
