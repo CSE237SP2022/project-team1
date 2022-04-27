fa90dd9c6a8347b217b0c3371656ffddb3edcea6
Maddie Hoffmann <e.hoffmann@wustl.edu>
2022-04-26 20:30:35 -0500

    resolved conflicts

--- src/promptClass/Prompt.java
+++ src/promptClass/Prompt.java
@@ -11,9 +11,12 @@ import foodClass.Food;
 import mealClass.Meal;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
 
 public class Prompt {
 	
